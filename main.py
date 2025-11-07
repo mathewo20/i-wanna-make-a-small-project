28b2698d4cf7a728b9586b614236cdf5dbbfc5c2
mathewo20 <mo237704937@gmail.com>
2025-11-07 13:58:22 +0000

    teacher did help me

---
+++ main.py
@@ -0,0 +1,7 @@
+friends = ['chris' , 'juan' , 'johnny' , 'jhorrdy' , 'jaden']
+
+for friend in friends:
+    print("happy new years " +  friend ) 
+    if len(friend) > 5:
+        print("woah you have a long name ")
+    print()
